73fa376292b276632d0cff51fa5f245fbc39f311
yufei <yufei0205@gmail.com>
2025-07-03 15:12:21 +0800

    初始配置

---
+++ src/main/java/com/whu/hongjing/HongjingApplication.java
@@ -0,0 +1,13 @@
+package com.whu.hongjing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongjingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongjingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whu/hongjing/HongjingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whu.hongjing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongjingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
