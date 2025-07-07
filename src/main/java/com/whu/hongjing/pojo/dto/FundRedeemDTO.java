e435c2ad23fdafe658e17da2cf416d5d64c629e6
yufei <yufei0205@gmail.com>
2025-07-07 13:55:33 +0800

    1.4.6 更新了先M 后RF的标签计算和虚拟线程批量刷新机制 也因此升级了框架到JDK21

--- src/main/java/com/whu/hongjing/pojo/dto/FundRedeemDTO.java
+++ src/main/java/com/whu/hongjing/pojo/dto/FundRedeemDTO.java
@@ -2,9 +2,9 @@ package com.whu.hongjing.pojo.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
